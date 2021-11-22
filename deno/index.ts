339192bbc26194dccc1432b92d3546c9645238e0
TheSacredLipton <44257207+TheSacredLipton@users.noreply.github.com>
2021-11-22 23:04:06 +0900

    Change to deno only

--- deno/index.ts
+++
@@ -1,3 +0,0 @@
-export { Either, Left, Right, isLeft, isRight } from './either/either.ts';
-export { Option, Some, None, isSome, isNone } from './option/option.ts';
-export { Result, Ok, Err, isOk, isErr } from './result/result.ts';
---
+++ mod.ts
@@ -0,0 +1,7 @@
+export { isLeft, isRight, Left, Right } from "./either.ts";
+export { isNone, isSome, None, Some } from "./option.ts";
+export { Err, isErr, isOk, Ok } from "./result.ts";
+
+export type { Either } from "./either.ts";
+export type { Option } from "./option.ts";
+export type { Result } from "./result.ts";
